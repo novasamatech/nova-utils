120e045b8186ae547aa41c7626ddc7fce88aa3f0
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-02-18 13:56:30 +0300

    fix readme allocation

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -8,34 +8,6 @@ from pytablewriter import MarkdownTableWriter
 
 
 readme = Template("""
-# Nova Utils
-
-## About
-This repo contains static information (logos, links, etc) to support client apps in Polkadot & Kusama ecosystem (e.g. [Nova Wallet]) to map it to the keys/ids from the network itself.
-
-Note: For better UX in your app its recommended to
-1. prepare UI states & logic when this information cannot be fetched due to github unavailability
-2. cache the data to cover part of the issue of 1.
-
-## Modules
-#### Crowdloan
-Crowdloans can be saturated with the following information:
-* id (to map the static data)
-* Parachain name
-* Description
-* Logo
-* Token
-* Website
-* Reward rate (KSM multiplier)
-
-#### Chains
-Contains JSON file with networks info: its token (ticker, precision), types, available nodes, account prefix, set of options (is testnet, has crowdloans)
-
-#### Icons
-Group of icons to saturate Nova Wallet
-
-Note: Icons should be used from trusted sources, however currently icons are not available on the participants' websites, so for convenience, there is /icons folder.
-
 {{networks_table}}
 """)
 
@@ -87,9 +59,8 @@ def parse_parameters(key_param, object):
 
 
 if __name__ == '__main__':
-    # cd into the script directory.
 
-    with open("README.md", "w") as f:
+    with open("./chains/README.md", "w") as f:
         f.write(readme.render(
             networks_table=generate_networks_table()
         ))
