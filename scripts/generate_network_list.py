989774f70f34cd4d1b943cdc354bad55d9d6a01c
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-04-18 12:11:46 +0300

    Improve network list

--- scripts/generate_network_list.py
+++ scripts/generate_network_list.py
@@ -13,10 +13,11 @@ readme = Template("""
 
 CHAINS_VERSION = "v3"
 
+
 def generate_networks_table():
     writer = MarkdownTableWriter(
         table_name="List of supported networks",
-        headers=["Network", "Assets", "Explorers", "History", "Staking"],
+        headers=["Network", "Assets", "Explorers", "SubQuery explorer"],
         value_matrix=generate_value_matrix(),
         margin=1
     )
@@ -37,8 +38,6 @@ def generate_value_matrix():
             "name", network.get("explorers")))
         network_data_array.append(parse_parameters(
             "history", network.get("externalApi")))
-        network_data_array.append(parse_parameters(
-            "staking", network.get("externalApi")))
         returning_array.append(network_data_array)
     return returning_array
 
@@ -51,12 +50,28 @@ def parse_parameters(key_param, object):
         data = object.get(key_param, " - ")
         if type(data) is str:
             return data
-        return "[{name}]({link})".format(name=data.get("type"), link=data.get("url"))
+        return subquery_url_formator(data.get("url"))
 
     return_data = '<br />'.join(str(x.get(key_param)) for x in object)
     if return_data is None:
         return " - "
     return return_data
+
+
+def subquery_url_formator(url):
+    explorer_base_url = "https://explorer.subquery.network/subquery/nova-wallet/"
+
+    if ("gapi" in url):
+        organisation = "nova-"
+        injecting_part = url.split("-")[1].split(".")[0]
+        final_explorer_url = "[{name}]({link})".format(
+            name=organisation+injecting_part, link=explorer_base_url + organisation + injecting_part)
+        return final_explorer_url
+    else:
+        injecting_part = url.split("/")[-1]
+        final_explorer_url = "[{name}]({link})".format(
+            name=injecting_part, link=explorer_base_url + injecting_part)
+        return final_explorer_url
 
 
 if __name__ == '__main__':
