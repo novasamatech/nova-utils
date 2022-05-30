4fe6f4d029b3d37caf497ffdd0d78ceda5fa2b28
leohar <leohar@rambler.ru>
2022-05-30 22:41:33 +0300

    Add workaround for missing ssformat

--- tests/test_network_parameters.py
+++ tests/test_network_parameters.py
@@ -1,6 +1,6 @@
 import json
 import pytest
-from scripts.create_type_file import get_properties, create_connection_by_url
+from scripts.create_type_file import get_properties, create_connection_by_url, get_metadata_param
 
 with open("../chains/v3/chains.json") as fin:
     chains_data = json.load(fin)
@@ -10,13 +10,16 @@ for chain in chains_data:
     data = (chain['nodes'][0]['url'], chain['assets'][0]['precision'], chain['addressPrefix'], chain['chainId'])
     test_data.append(data)
 
-
 @pytest.mark.parametrize("url, expected_precision, expected_address_format, expected_genesis", test_data)
 def test_properties(url, expected_precision, expected_address_format, expected_genesis):
-    network_property = get_properties(create_connection_by_url(url))
+    substrate = create_connection_by_url(url)
+    network_property = get_properties(substrate)
     if isinstance(network_property.precision, list):
         assert network_property.precision[0] == expected_precision
     else:
         assert network_property.precision == expected_precision
-    assert network_property.ss58Format == expected_address_format
+    if network_property.ss58Format is None:
+        assert substrate.ss58_format == expected_address_format
+    else:
+        assert network_property.ss58Format == expected_address_format
     assert network_property.chainId[2:] == expected_genesis
