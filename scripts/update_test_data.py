fbfce6567d7ab2145fb2c20fa6678bc67e06ac1d
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-08-25 09:35:52 +0300

    Update test data script

--- scripts/update_test_data.py
+++ scripts/update_test_data.py
@@ -14,7 +14,7 @@ def get_ids(chains):
     return list(map(lambda x: x["chainId"], chains))
 
 
-exludeChains = ['Kintsugi', 'Statemint', 'Singular testnet', 'Interlay', 'Mangata X', 'Arctic Relay Testnet', 'Rococo Testnet', 'Imbue', 'Tinkernet']
+exludeChains = ['Kintsugi', 'Singular testnet', 'Interlay', 'Mangata X', 'Arctic Relay Testnet', 'Rococo Testnet']
 
 dev_ids = get_ids(dev_chains)
 test_ids = get_ids(test_chains)
