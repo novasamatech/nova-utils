994d5a3e09834587542c9390aa07406ed929a687
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-03-20 18:33:12 +0300

    change implementation

--- xcm/update_to_prod.py
+++ xcm/update_to_prod.py
@@ -1,109 +1,130 @@
 import json
 
-def convert_list_to_dict(lst, key_name):
-    return_dict = {}
-    for _, item in enumerate(lst):
-        if key_name == 'xcmTransfers':
-            return_dict[item["destination"]["chainId"]] = item
-        else:
-            return_dict[item[key_name]] = item
-    return return_dict
+def ask_to_update():
+    update = input("Would you like to update that data? (y/n)")
+    if update.lower() == 'y':
+        return True
+    return False
 
-def convert_dict_to_list(dct):
-    return [element for element in dct]
+def update_networks(dev, prod, meta_dict):
+    for key, value in dev.items():
+        if key not in prod:
+            print(f"Was added new chain - {meta_dict[key]['name']}")
+            if ask_to_update():
+                prod[key] = value
 
-def compare_data(data1, data2, path="", check_modes=True, add_keys=True):
-    """
-    Recursively compare data1 and data2 dictionaries and prints the
-    differences to the console. Returns a dictionary of updates.
-    """
-    updates = {}
-    for key, value1 in data1.items():
-        value2 = data2.get(key)
-        new_path = path + "/" + key if path else key
-        if 'networkBaseWeight' in new_path.split('/'):
-            continue
-        if 'assetsLocation' in new_path.split('/'):
-            continue
-        if 'instructions' in new_path.split('/'):
-            continue
-        if isinstance(value1, dict) and isinstance(value2, dict):
-            nested_updates = compare_data(value1, value2, new_path, check_modes, add_keys)
-            if nested_updates:
-                updates.update(nested_updates)
-        elif isinstance(value1, list) and isinstance(value2, list):
-            if key == 'chains':
-                key_name = 'chainId'
-            elif key == 'assets':
-                key_name = 'assetLocation'
-            elif key == 'xcmTransfers':
-                key_name = 'xcmTransfers'
-            else:
-                raise KeyError(f'List {key} is unknown, please add maping to convert it to dict.')
+    return prod
 
-            dict1 = convert_list_to_dict(value1, key_name)
-            dict2 = convert_list_to_dict(value2, key_name)
-            for i, item in dict1.items():
-                if i not in dict2:
-                    if add_keys:
-                        dict2[i] = {}
-                    else:
-                        continue
-                nested_updates = compare_data(item, dict2[i], f"{new_path}[{i}]", check_modes, add_keys)
-                if nested_updates:
-                    updates.update(nested_updates)
-            if len(dict1) != len(dict2):
-                print(f"\nData in file_1.json has a different number of items than in xcm/v2/transfers.json for {new_path}")
-                print(f"File 1 items: {len(dict1)}")
-                print(f"File 2 items: {len(dict2)}")
-                if add_keys:
-                    update = input("Which value should be updated in xcm/v2/transfers.json? (y/n) ")
-                    if update.lower() == 'y':
-                        updates[new_path] = value1
-                    dict2.update({i: None for i in range(len(dict2), len(dict1))})
-            value1 = convert_dict_to_list(dict1)
-            value2 = convert_dict_to_list(dict2)
-        elif value2 != value1:
-            if check_modes is False and 'mode' in new_path.split('/'):
-                continue
-            print(f"Data in file_1.json updated for {new_path}")
-            print(f"Old value: {value2}")
-            print(f"New value: {value1}")
-            update = input("Which value should be updated in xcm/v2/transfers.json? (y/n) ")
-            if update.lower() == 'y':
-                updates[new_path] = value1
-    return updates
+def update_network_base_weight(dev_file, prod_file, meta_dict):
+    for chain_id, weight in dev_file['networkBaseWeight'].items():
+        if chain_id not in prod_file['networkBaseWeight']:
+            print(f"Was added new base weight in chain: {meta_dict[chain_id]['name']}")
+            if ask_to_update():
+                prod_file['networkBaseWeight'][chain_id] = weight
+
+    return prod_file['networkBaseWeight']
 
 
-# Read data from file_1.json and file_2.json
-with open('xcm/v2/transfers_dev.json') as f:
-    data1 = json.load(f)
-with open('xcm/v2/transfers.json') as f:
-    data2 = json.load(f)
+def update_reserves(dev_file, prod_file):
+    for asset_location, asset in dev_file['assetsLocation'].items():
+        if asset_location not in prod_file['assetsLocation']:
+            print(f"Was added new asset: {asset_location}")
+            if ask_to_update():
+                prod_file['assetsLocation'][asset_location] = asset
 
-# Ask for mode of operation
-mode = input("Enter mode of operation (1 = check all changes, 2 = skip 'mode' paths): ")
-if mode == '1':
-    check_modes = True
-elif mode == '2':
-    check_modes = False
-else:
-    print("Invalid mode selected. Defaulting to mode 1 (check all changes).")
-    check_modes = True
+    return prod_file['assetsLocation']
 
-# Compare the data in the two files
-updates = compare_data(data1, data2, check_modes=check_modes)
 
-# Update xcm/v2/transfers.json
-if updates:
-    for key, value in updates.items():
-        keys = key.split("/")
-        obj = data2
-        for k in keys[:-1]:
-            obj = obj[k]
-        obj[keys[-1]] = value
+def update_assets(dev, prod, meta_dict):
+    for chain_id, _ in prod.items():
+        for asset_location, asset in dev[chain_id]['assets'].items():
+            if asset_location \
+                not in prod[chain_id]['assets']:
+                print(f"Was added new asset {asset_location} in chain: {meta_dict[chain_id]['name']}")
+                if ask_to_update():
+                    prod[chain_id]['assets'][asset_location] = asset
+
+    return prod
+
+
+def update_destinations(dev, prod, meta_dict):
+    for chain_id, chain in prod.items():
+        for asset_location, asset in chain['assets'].items():
+            for destination_id, destination in dev[chain_id]['assets'][asset_location]['destinations'].items():
+                if destination_id not in asset['destinations']:
+                    print(f"Was added new destination in {meta_dict[chain_id]['name']} \
+                          \nfor asset: {asset_location} \
+                          \nto network: {meta_dict[destination_id]['name']} ")
+                    if ask_to_update():
+                        prod[chain_id]['assets'][asset_location][destination_id] = destination
+
+    return prod
+
+
+def conver_chains_to_dict(chains_obj):
+    chins_dict = {chain['chainId']: chain for chain in chains_obj}
+    for _, chain in chins_dict.items():
+        assets_dict = {}
+        for asset in chain['assets']:
+            destinations_dict = {destination['destination']['chainId']: destination for destination in asset['xcmTransfers']}
+            assets_dict[asset['assetLocation']] = asset
+            assets_dict[asset['assetLocation']]['destinations'] = destinations_dict
+        chain['assets'] = assets_dict
+
+    return chins_dict
+
+def convert_chain_dict_to_array_back(chains_dict):
+    temp_chains = []
+    for _, chain in chains_dict.items():
+        temp_assets = []
+        for _, asset in chain['assets'].items():
+            temp_destinations = []
+            for _, destination in asset['destinations'].items():
+                temp_destinations.append(destination)
+            asset['destinations'] = temp_destinations
+            temp_assets.append(asset)
+        chain['assets'] = temp_assets
+        temp_chains.append(chain)
+
+    return temp_chains
+
+
+
+def promote_updates_to_prod(dev_file, prod_file, meta_data):
+    meta_dict = {chain['chainId']: chain for chain in meta_data}
+
+    dev_chains_dict = conver_chains_to_dict(dev_file['chains'])
+    prod_chains_data = conver_chains_to_dict(prod_file['chains'])
+
+    updated_prod = update_networks(dev_chains_dict, prod_chains_data, meta_dict)
+    updated_prod = update_assets(dev_chains_dict, updated_prod, meta_dict)
+    updated_prod = update_destinations(dev_chains_dict, updated_prod, meta_dict)
+
+    updated_chains = convert_chain_dict_to_array_back(prod_chains_data)
+
+    updated_base_weight = update_network_base_weight(dev_file, prod_file, meta_dict)
+
+    updated_reserves = update_reserves(dev_file, prod_file)
+
+    prod_file['networkBaseWeight'] = updated_base_weight
+    prod_file['assetsLocation'] = updated_reserves
+    prod_file['chains'] = updated_chains
+
+    return prod_file
+
+
+if __name__ == "__main__":
+    # Read data from files
+    with open('xcm/v2/transfers_dev.json') as f:
+        data1 = json.load(f)
+    with open('xcm/v2/transfers.json') as f:
+        data2 = json.load(f)
+    with open('chains/v8/chains_dev.json') as f:
+        meta_data = json.load(f)
+
+
+    updated_obj = promote_updates_to_prod(dev_file=data1, prod_file=data2, meta_data=meta_data)
+
     with open('xcm/v2/transfers.json', 'w') as f:
-        json.dump(data2, f, indent=4)
+        json.dump(updated_obj, f, indent=2)
         print("Data updated in xcm/v2/transfers.json")
-else:
-    print("No updates needed")
