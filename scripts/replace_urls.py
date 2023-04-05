c9b65f1c4443dd901dfac9953443ecf85d9c2f12
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-04-05 11:10:44 +0300

    update implementation in order to reuse it

--- scripts/replace_urls.py
+++ scripts/replace_urls.py
@@ -1,37 +1,39 @@
 import os
 import json
 
-def replace_url(json_data, value_to_replace, new_value):
+def replace_data_in_nested_object(json_data, value_to_replace, new_value):
     """
     Recursively replaces value in any parameter of dictionaries with new_value.
     """
     if isinstance(json_data, dict):
         for key, value in json_data.items():
             if isinstance(value, (dict, list)):
-                replace_url(value, value_to_replace, new_value)
+                replace_data_in_nested_object(value, value_to_replace, new_value)
             elif isinstance(value, str) and value_to_replace in value:
                 json_data[key] = value.replace(value_to_replace, new_value)
     elif isinstance(json_data, list):
         for item in json_data:
-            replace_url(item, value_to_replace, new_value)
+            replace_data_in_nested_object(item, value_to_replace, new_value)
 
-env = os.getenv('ENVIRONMENT')
-new_domain = 'https://config2.novasama.uz/'
-value_to_replace = 'https://raw.githubusercontent.com/nova-wallet/nova-utils/master/'
 
-if env == 'DEV':
-    chains_file_path = os.getenv('DEV_CHAINS_JSON_PATH')
-elif env == 'PROD':
-    chains_file_path = os.getenv('CHAINS_JSON_PATH')
-else:
-    raise Exception(f'Provide right ENVIRONMENT variable - DEV or PROD, currentli it is: {env}')
+if __name__ == '__main__':
+    env = os.getenv('ENVIRONMENT')
+    new_domain = 'https://config2.novasama.uz/'
+    value_to_replace = 'https://raw.githubusercontent.com/nova-wallet/nova-utils/master/'
 
-replaced_file_path = chains_file_path # Temporarly set the same path to upda provided file
+    if env == 'DEV':
+        chains_file_path = os.getenv('DEV_CHAINS_JSON_PATH')
+    elif env == 'PROD':
+        chains_file_path = os.getenv('CHAINS_JSON_PATH')
+    else:
+        raise Exception(f'Provide right ENVIRONMENT variable - DEV or PROD, currentli it is: {env}')
 
-with open(chains_file_path, 'r') as f:
-    data = json.load(f)
+    replaced_file_path = chains_file_path # Temporarly update the same file as provided
 
-replace_url(data, new_domain)
+    with open(chains_file_path, 'r') as f:
+        data = json.load(f)
 
-with open(replaced_file_path, 'w') as f:
-    f.write(json.dumps(data, indent=4))
+    replace_data_in_nested_object(data, new_domain)
+
+    with open(replaced_file_path, 'w') as f:
+        f.write(json.dumps(data, indent=4))
