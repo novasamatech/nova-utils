d046cb36a2f14e859dbabe00430a818c7e8ed85d
leohar <leohar@rambler.ru>
2023-04-19 13:01:49 +0800

    added delayed assert for several test failures

--- tests/test_check_new_assets.py
+++ tests/test_check_new_assets.py
@@ -1,4 +1,5 @@
 import pytest
+import delayed_assert
 from scripts.utils.chain_model import Chain
 from tests.data.setting_data import chains
 
@@ -11,13 +12,14 @@ task_ids = [
 @pytest.mark.parametrize("chain", chains, ids=task_ids)
 class TestAssets:
     def test_has_new_assets(self, chain: Chain):
+        added_assets = []
+        for asset in chain.assets:
+            added_assets.append(asset['symbol'].upper())
         chain.create_connection()
         chain.init_properties()
-        assets = []
-        for asset in chain.assets:
-            assets.append(asset['symbol'].upper())
         if isinstance(chain.substrate.token_symbol, list):
             for symbol in chain.substrate.token_symbol:
-                assert symbol in assets
+                delayed_assert.expect(symbol in added_assets, "new token to add: " + symbol)
         else:
-            assert chain.properties.symbol == chain.assets[0]['symbol']
+            assert chain.assets[0]['symbol'] == chain.properties.symbol, "native asset has changed"
+        delayed_assert.assert_expectations()
