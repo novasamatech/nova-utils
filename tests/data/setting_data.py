40b0a7a65170ce3ebde12ac9c6f724e66a198586
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-04-19 08:02:36 +0300

    Adding test for eth nodes

--- tests/data/setting_data.py
+++ tests/data/setting_data.py
@@ -1,11 +1,42 @@
 import os
+
+from typing import List
 from scripts.utils.work_with_data import get_network_list
 from scripts.utils.chain_model import Chain
 
-network_file_path = os.getenv('CHAINS_JSON_PATH', "chains/v6/chains.json")
+network_file_path = os.getenv('CHAINS_JSON_PATH', "chains/v10/chains.json")
 skipped_networks = ['Edgeware', 'Ethereum']
+network_list = get_network_list('/'+ network_file_path)
 
-global chains
+chains = [Chain(data) for data in network_list if data.get('name') not in skipped_networks]
 
-chains = [Chain(data) for data in get_network_list('/'+
-        network_file_path) if data.get('name') not in skipped_networks]
+def get_ethereum_chains():
+    eth_chains = []
+    for data in network_list:
+        options = data.get('options')
+        if options is not None and 'ethereumBased' in options:
+            eth_chains.append(Chain(data))
+            
+    return eth_chains
+    
+
+def collect_nodes_for_chains(networks: List[Chain]):
+    result = []
+    for network in networks:
+        for node in network.nodes:
+            result.append(
+                {"url": node.get("url"), "name": network.name})
+
+    return result
+
+
+def collect_rpc_nodes_for_chains(networks: List[Chain]):
+	result = []
+	for network in networks:
+		for node in network.nodes:
+			if 'https' in node.get("url"):
+				result.append(
+					{"url": node.get("url"), "name": network.name}
+				)
+	
+	return result
--- tests/test_nodes_availability.py
+++ tests/test_nodes_availability.py
@@ -1,21 +1,10 @@
 
-from typing import List
 import pytest
 
 from substrateinterface import SubstrateInterface
-from tests.data.setting_data import chains
+from tests.data.setting_data import chains, collect_nodes_for_chains
 from scripts.utils.substrate_interface import create_connection_by_url
-from scripts.utils.chain_model import Chain
 
-
-def collect_nodes_for_chains(networks: List[Chain]):
-    result = []
-    for network in networks:
-        for node in network.nodes:
-            result.append(
-                {"url": node.get("url"), "name": network.name})
-
-    return result
 
 task_ids = [
     f'Test for {task["name"]}, url: {task["url"]}'
