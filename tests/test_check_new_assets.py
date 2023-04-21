1a3721b958ecbc3945a4572ec805a9a667d8b97a
leohar <leohar@rambler.ru>
2023-04-21 15:14:08 +0800

    fix exclusion assertion for native tokens

--- tests/test_check_new_assets.py
+++ tests/test_check_new_assets.py
@@ -27,5 +27,5 @@ class TestAssets:
             for symbol in chain.substrate.token_symbol:
                 delayed_assert.expect(symbol in chain_assets.keys() or symbol in chain_assets.values(), "new token to add: " + symbol)
         else:
-            assert chain.assets[0]['symbol'] == chain.properties.symbol, "native asset has changed"
+            assert chain.properties.symbol in chain_assets.keys() or chain.properties.symbol in chain_assets.values(), "native asset has changed"
         delayed_assert.assert_expectations()
