e839b69a01465ec9b8a5f40345e85c3cbc97d232
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-04-22 10:23:06 +0300

    fix actual json path script

--- .github/get_actual_json_path.sh
+++ .github/get_actual_json_path.sh
@@ -2,6 +2,21 @@
 SCRIPT_PATH=$(dirname "$0")
 MAIN_DIRECTORY=${SCRIPT_PATH%/*}
 
-folders=($(ls ${MAIN_DIRECTORY}/../$1))
+# Retrieve the folder names and extract the version numbers
+folders=($(ls -1 ${MAIN_DIRECTORY}/../$1))
+version_numbers=()
+for folder in "${folders[@]}"; do
+  # Extract the version number by removing the "v" prefix and any non-numeric characters
+  version_number=$(echo ${folder#v} | tr -dc '[:digit:]')
+  # Append the extracted version number to the array
+  version_numbers+=($version_number)
+done
 
-echo ${folders[-1]}
+# Sort the version numbers in descending order
+sorted_version_numbers=($(printf '%s\n' "${version_numbers[@]}" | sort -rn))
+
+# Retrieve the latest version folder
+latest_version_number=${sorted_version_numbers[0]}
+latest_version_folder="v${latest_version_number}"
+
+echo $latest_version_folder
