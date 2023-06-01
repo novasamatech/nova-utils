7d02c1d1a3b8d71b5654b8898468eac60be78e50
leohar <leohar@rambler.ru>
2023-06-01 15:47:11 +0500

    combine assets mapping into exclusions to avoid generalisation

--- tests/test_check_new_assets.py
+++ tests/test_check_new_assets.py
@@ -11,20 +11,21 @@ task_ids = [
 
 @pytest.mark.parametrize("chain", get_substrate_chains(), ids=task_ids)
 class TestAssets:
-    # workaround to differences between asset names at runtime and in our configuration
-    asset_mapping = {
-        'AUSD': 'KUSD',
-        'XX': 'xx'
-    }
-    # assets that has no working cases on network
+    # assets that have no working cases or having differences between
+    # asset names at runtime and in our configuration
     exclusions = {
-        'Bifrost Kusama': {'DOT': ''},
-        'Kintsugi': {'INTR': '', 'iBTC': 'IBTC', 'DOT': ''}}
-    
+        'Karura': {'AUSD': 'KUSD'},
+        'Acala': {'AUSD': 'KUSD'},
+        'Shiden': {'AUSD': 'KUSD'},
+        'Bifrost Kusama': {'AUSD': 'KUSD', 'DOT': ''},
+        'Kintsugi': {'AUSD': 'KUSD', 'INTR': '', 'iBTC': 'IBTC', 'DOT': ''},
+        'Basilisk': {'AUSD': 'KUSD'},
+        'Turing': {'AUSD': 'KUSD'},
+        'XX network': {'XX': 'xx'}}
+
     def test_has_new_assets(self, chain: Chain):
-        
+
         chain_assets = {asset['symbol'].upper(): '' for asset in chain.assets}
-        chain_assets.update(self.asset_mapping)
         if chain.name in self.exclusions.keys():
             for key, value in self.exclusions.items():
                 if key == chain.name:
@@ -33,7 +34,8 @@ class TestAssets:
         chain.init_properties()
         if isinstance(chain.substrate.token_symbol, list):
             for symbol in chain.substrate.token_symbol:
-                delayed_assert.expect(symbol in chain_assets.keys() or symbol in chain_assets.values(), "new token to add: " + symbol)
+                delayed_assert.expect(symbol in chain_assets.keys() or symbol in chain_assets.values(),
+                                      "new token to add: " + symbol)
         else:
             assert chain.properties.symbol in chain_assets.keys() or chain.properties.symbol in chain_assets.values(), "native asset has changed"
         delayed_assert.assert_expectations()
