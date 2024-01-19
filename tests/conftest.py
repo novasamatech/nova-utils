828d4bfc94d65ec0be4a976e345084eff1497fba
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-01-19 10:48:57 +0300

    update implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from scripts.utils.metadata_interaction import get_properties
 from scripts.utils.substrate_interface import create_connection_by_url
 from tests.data.setting_data import collect_nodes_for_chains, get_substrate_chains
 
+FIXTURE_TIMEOUT = 10
 
 chain_names = [
     f'Test for {task.name}'
@@ -16,21 +17,42 @@ task_ids = [
     f'Test for {task["name"]}, url: {task["url"]}'
     for task in collect_nodes_for_chains(get_substrate_chains())
 ]
+def execute_with_timeout(timeout: int, function: function, args: tuple, error_message: str):
+    try:
+        return func_timeout(timeout=timeout, func=function, args=args)
+    except FunctionTimedOut:
+        pytest.fail(msg=error_message)
 
 @pytest.fixture(scope="module", params=collect_nodes_for_chains(get_substrate_chains()), ids=task_ids)
 def connection_by_url(request):
-    timeout = 10
     data = request.param
-    try:
-        connection = func_timeout(timeout, create_connection_by_url, args=(data["url"],))
-        func_timeout(timeout, get_properties, args=(connection,))
-        return connection
-    except FunctionTimedOut:
-        pytest.fail(f'Reach timeout {timeout} for create connection on: {data["url"]}')
+    connection = execute_with_timeout(
+        timeout=FIXTURE_TIMEOUT,
+        function=create_connection_by_url,
+        args=(data["url"],),
+        error_message=f'Timeout {FIXTURE_TIMEOUT} when creating connection: {data["url"]}'
+    )
+    execute_with_timeout(
+        timeout=FIXTURE_TIMEOUT,
+        function=get_properties,
+        args=(connection,),
+        error_message=f'Timeout {FIXTURE_TIMEOUT} when getting properties: {data["url"]}'
+    )
+    return connection
 
 @pytest.fixture(scope="module", params=get_substrate_chains(), ids=chain_names)
 def chain_model(request) -> Chain:
     chain = request.param
-    chain.create_connection()
-    chain.init_properties()
+    execute_with_timeout(
+        timeout=FIXTURE_TIMEOUT,
+        function=chain.create_connection,
+        args=(),
+        error_message=f'Timeout {FIXTURE_TIMEOUT} when creating connection: {chain.name}'
+    )
+    execute_with_timeout(
+        timeout=FIXTURE_TIMEOUT,
+        function=chain.init_properties,
+        args=(),
+        error_message=f'Timeout {FIXTURE_TIMEOUT} when initializing properties: {chain.name}'
+    )
     return chain
