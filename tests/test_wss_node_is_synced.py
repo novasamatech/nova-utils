90d1afffcdb337724d47277bd9b9d820f8a2a511
leohar <leohar@rambler.ru>
2024-08-28 18:53:34 +0100

    add test, add indents

---
+++ tests/test_wss_node_is_synced.py
@@ -0,0 +1,16 @@
+from substrateinterface import SubstrateInterface
+
+
+def test_rpc_node_is_synced(connection_by_url: SubstrateInterface):
+    sync_state = connection_by_url.rpc_request(
+        method='system_syncState',
+        params=[],
+    ).get("result")
+
+    if sync_state:
+        current_block = sync_state['currentBlock']
+        highest_block = sync_state['highestBlock']
+
+        assert highest_block - current_block <= 1
+    else:
+        assert False, "Failed to retrieve SyncState"
