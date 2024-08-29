86f3a4914de0aed608f3753d6d7a4fbac776034f
leohar <leohar@rambler.ru>
2024-08-29 17:16:09 +0100

    update to handle Method not found Exception

--- tests/test_substrate_node_is_synced.py
+++ tests/test_substrate_node_is_synced.py
@@ -1,16 +1,23 @@
+import time
+
 from substrateinterface import SubstrateInterface
+from substrateinterface.exceptions import SubstrateRequestException
 
 
 def test_rpc_node_is_synced(connection_by_url: SubstrateInterface):
-    sync_state = connection_by_url.rpc_request(
-        method='system_syncState',
-        params=[],
-    ).get("result")
+    try:
+        sync_state = connection_by_url.rpc_request(
+            method='system_syncState',
+            params=[],
+        ).get("result")
+        if sync_state:
+            current_block = sync_state['currentBlock']
+            highest_block = sync_state['highestBlock']
+            assert highest_block - current_block <= 300  # 30 min = 1 block ~ 6s * 300
 
-    if sync_state:
-        current_block = sync_state['currentBlock']
-        highest_block = sync_state['highestBlock']
-
-        assert highest_block - current_block <= 100
-    else:
-        assert False, "Failed to retrieve SyncState"
+        else:
+            assert False, "Failed to retrieve SyncState"
+    except SubstrateRequestException as err:
+        # If we catch Method not found Exception -> then check internal network time
+        system_timestamp = connection_by_url.query("Timestamp", "Now").value / 1000
+        assert abs(time.time() - system_timestamp) < 1800  # 30 min = 30 * 60s
