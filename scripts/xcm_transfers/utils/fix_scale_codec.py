a55f83acf3199cb2502261181c5631acc3ec9d84
valentunn <70131744+valentunn@users.noreply.github.com>
2025-04-01 10:15:08 +0300

    Feature/legacy xcm file cleanup (#3569)
    * Script to clean up legacy file; Fix module errors lookup
    
    * Run cleanup script
    
    * Apply automatic changes

--- scripts/xcm_transfers/utils/fix_scale_codec.py
+++ scripts/xcm_transfers/utils/fix_scale_codec.py
@@ -1,5 +1,12 @@
-from scalecodec import ScaleBytes, Tuple as ScaleTuple
+from scalecodec import ScaleBytes, Tuple as ScaleTuple, GenericMetadataVersioned
 
+
+def fix_substrate_interface():
+    fix_tuple_encoding()
+    fix_metadata_error_search()
+
+def fix_tuple_encoding():
+    ScaleTuple.process_encode = process_encode
 
 # This is a modified implementation of Tuple.process_encode that fixes issue https://github.com/polkascan/py-scale-codec/issues/126
 def process_encode(self: ScaleTuple, value):
@@ -24,5 +31,20 @@ def process_encode(self: ScaleTuple, value):
 
     return data
 
-def fix_scale_codec():
-    ScaleTuple.process_encode = process_encode
+def fix_metadata_error_search():
+    GenericMetadataVersioned.get_module_error = get_module_error
+
+# This is a modified implementation of GenericMetadataVersioned.get_module_error that accounts for non-continuous error indices
+def get_module_error(self, module_index, error_index):
+    if self.portable_registry:
+        for module in self.pallets:
+            if module['index'] == module_index and module.errors:
+                # CHANGED
+                # return module.errors[error_index]
+                for error in module.errors:
+                    if error['index'] == error_index:
+                        return error
+
+                raise Exception(f"Error {error_index} was not found in module ${module["name"]}")
+    else:
+        return self.value_object[1].error_index.get(f'{module_index}-{error_index}')
