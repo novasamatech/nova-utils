00f224cb96d1fa7c6975a1670a29c155aa36c650
valentunn <70131744+valentunn@users.noreply.github.com>
2025-04-07 10:33:36 +0300

    Rework access_substrate recovery (#3588)
    * Cover another reconnect case
    
    * Rework approach to break access_substrate nestendess

--- scripts/utils/chain_model.py
+++ scripts/utils/chain_model.py
@@ -8,6 +8,7 @@ from substrateinterface import SubstrateInterface
 
 from .metadata_interaction import get_properties
 from .substrate_interface import create_connection_by_url
+from ..xcm_transfers.utils.log import debug_log
 
 T = TypeVar('T')
 
@@ -23,6 +24,8 @@ class Chain:
     _type_registry: dict
 
     _type_cache: dict = {}
+
+    _access_substrate_counter: int = 0
 
     def __init__(self, arg, type_registry=None):
         self.chainId = arg.get("chainId")
@@ -100,15 +103,19 @@ class Chain:
             self.create_connection()
 
         try:
+            self._access_substrate_counter += 1
             return action(self.substrate)
         except Exception as e:
-            if self._is_connection_broken_error(e):
-                print("Attempting to re-create connection after broken connection", e)
+            if self._access_substrate_counter == 1:
+                print("Attempting to re-create connection after broken connection:", e)
                 # try re-connecting socket and performing action once again
                 self.recreate_connection()
                 return action(self.substrate)
             else:
+                debug_log("Nested access_substrate - propagate error to the outer-most level")
                 raise e
+        finally:
+            self._access_substrate_counter -= 1
 
     def encodable_address(self, account_id: str):
         if self.has_evm_addresses():
@@ -121,12 +128,6 @@ class Chain:
     def _uses_multi_address(self) -> bool:
         type_def = self.substrate.get_type_definition("Address")
         return type_def is dict
-
-    @staticmethod
-    def _is_connection_broken_error(e: Exception) -> bool:
-        message = str(e)
-        return message in {"Expecting value: line 1 column 1 (char 0)", "Failure Connection to remote host was lost."}
-
 
 class ChainAsset:
     _data: dict
