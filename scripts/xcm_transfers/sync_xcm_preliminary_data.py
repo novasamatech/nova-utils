1190ed776d5bf7370817eaf3c64feadc5f41dbfc
valentunn <70131744+valentunn@users.noreply.github.com>
2025-06-17 17:00:12 +0300

    Fix/mythos xcm (#3802)
    * Fix - Mythos is not detected as xcm capable chain
    
    * Code style

--- scripts/xcm_transfers/sync_xcm_preliminary_data.py
+++ scripts/xcm_transfers/sync_xcm_preliminary_data.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import json
-
 from typing import List
 
 from substrateinterface import SubstrateInterface
@@ -49,10 +48,12 @@ def determine_dry_run_version(substrate: SubstrateInterface, runtime_prefix: str
         if "DryRunApi_dry_run_xcm is not found" in error_message:
             return None
         # Dry run v2 has additional argument so it will result in a trap
-        elif "Execution aborted due to trap" in error_message:
+        elif "Execution aborted due to trap" in error_message or "Client error: Execution failed: Execution aborted due to panic" in error_message:
             return dry_run_v2
         # Something unknown went wrong so we skip
         else:
+            print("Unknown error:", e)
+
             return None
 
 def construct_v1_dry_run_method_data(substrate: SubstrateInterface, runtime_prefix: str)-> str:
